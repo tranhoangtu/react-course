47b58588a6a0813e1d66dafa7425e63087aa36ca
Tran Tu <tranhoangtu12091994@gmail.com>
2021-06-07 14:39:20 +0700

    assignment 2

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,10 +3,17 @@ import React from 'react';
 import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import './Expenses.css';
+import ExpensesFilter from './ExpensesFilter';
 
 const Expenses = (props) => {
+
+  const filterHandler = (year) => {
+    console.log(year);
+  }
+
   return (
     <Card className="expenses">
+    <ExpensesFilter onFilter={filterHandler} />
     {
       props.items.map(item => <ExpenseItem
         title={item.title}
---
+++ src/components/Expenses/ExpensesFilter.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+
+import './ExpensesFilter.css';
+
+const ExpensesFilter = (props) => {
+    const [year, setYear] = useState('');
+
+    const yearChangeHandler = (event) => {
+      setYear(event.target.value);
+      props.onFilter(event.target.value);
+    }
+    
+  return (
+    <div className='expenses-filter'>
+      <div className='expenses-filter__control'>
+        <label>Filter by year</label>
+        <select name='year' value={year} onChange={yearChangeHandler}>
+          <option value='2022'>2022</option>
+          <option value='2021'>2021</option>
+          <option value='2020'>2020</option>
+          <option value='2019'>2019</option>
+        </select>
+      </div>
+    </div>
+  );
+};
+
+export default ExpensesFilter;
