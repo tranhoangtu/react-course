30b681c84417dc269eeb33d0cd35147d5af49976
Tran Tu <tranhoangtu12091994@gmail.com>
2021-06-07 11:33:16 +0700

    Handle state with form in better way

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -4,20 +4,16 @@ import './ExpenseForm.css';
 
 const ExpenseForm = () => {
 
-  const [title, setTitle] = useState('');
-  const [amount, setAmount] = useState('');
-  const [date, setDate] = useState('');
+  const [userInput, setUserInput] = useState({title: '', amount: '', date: ''});
 
-  const titleChangeHandler = (event) => {
-    setTitle(event.target.value);
-  }
-
-  const amountChangeHandler = (event) => {
-    setAmount(event.target.value);
-  }
-
-  const dateChangeHandler = (event) => {
-    setDate(event.target.value);
+  const userInputChangeHandler = (event) => {
+    let name = event.target.name;
+    let value = event.target.value;
+    console.log(name);
+    setUserInput((prevState) => {return {
+      ...prevState,
+      [name]: value
+    }});
   }
 
   return (
@@ -25,15 +21,15 @@ const ExpenseForm = () => {
       <div className='new-expense__controls'>
         <div className='new-expense__control'>
           <label>Title</label>
-          <input type='text' onChange={titleChangeHandler} value={title} />
+          <input type='text' name='title' onChange={userInputChangeHandler} value={userInput.title} />
         </div>
         <div className='new-expense__control'>
           <label>Amount</label>
-          <input type='number' min='0.01' step='0.01' onChange={amountChangeHandler} value={amount} />
+          <input type='number' name='amount' min='0.01' step='0.01' onChange={userInputChangeHandler} value={userInput.amount} />
         </div>
         <div className='new-expense__control'>
           <label>Date</label>
-          <input type='date' min='2019-01-01' max='2022-12-31' onChange={dateChangeHandler} value={date} />
+          <input type='date' name='date' min='2019-01-01' max='2022-12-31' onChange={userInputChangeHandler} value={userInput.date} />
         </div>
       </div>
       <div className='new-expense__actions'>
