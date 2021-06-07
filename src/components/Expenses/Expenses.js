5d8d4030a710be244e19ffdfb81835dc25142fee
Tran Tu <tranhoangtu12091994@gmail.com>
2021-06-07 16:43:29 +0700

    Create expense list component

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,9 +1,8 @@
 import React, { useState } from 'react';
-
-import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import './Expenses.css';
 import ExpensesFilter from './ExpensesFilter';
+import ExpensesList from './ExpensesList';
 
 const Expenses = (props) => {
 
@@ -18,13 +17,7 @@ const Expenses = (props) => {
   return (
     <Card className="expenses">
     <ExpensesFilter selectedYear={filterYear} onFilter={filterHandler} />
-    {filterExpenses.length === 0 ? <p>No results.</p> : filterExpenses.map(item => <ExpenseItem
-        key={item.id}
-        id={item.id}
-        title={item.title}
-        amount={item.amount}
-        date={item.date}
-      />)}
+    <ExpensesList items={filterExpenses} />
     </Card>
   );
 }
