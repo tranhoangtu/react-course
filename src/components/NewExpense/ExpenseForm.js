7c3c683fc21aad80b6e95f08b20f13d87d219ca4
Tran Tu <tranhoangtu12091994@gmail.com>
2021-06-08 10:44:05 +0700

    fix bug add amount at submit handler

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,55 +1,81 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import './ExpenseForm.css';
+import "./ExpenseForm.css";
 
 const ExpenseForm = (props) => {
-
-  const [userInput, setUserInput] = useState({title: '', amount: '', date: ''});
+  const [userInput, setUserInput] = useState({
+    title: "",
+    amount: "",
+    date: "",
+  });
 
   const userInputChangeHandler = (event) => {
     let name = event.target.name;
     let value = event.target.value;
     console.log(name);
-    setUserInput((prevState) => {return {
-      ...prevState,
-      [name]: value
-    }});
-  }
+    setUserInput((prevState) => {
+      return {
+        ...prevState,
+        [name]: value,
+      };
+    });
+  };
 
-  const submitHandler = (event) =>{
+  const submitHandler = (event) => {
     event.preventDefault();
 
     let submitInput = {
       title: userInput.title,
-      amount: userInput.amount,
-      date: new Date(userInput.date)
-    }
+      amount: +userInput.amount,
+      date: new Date(userInput.date),
+    };
 
     props.onSaveExpenseData(submitInput);
-  }
+  };
   const cancelHandler = () => {
     props.onCancel();
-  }
+  };
 
   return (
     <form onSubmit={submitHandler}>
-      <div className='new-expense__controls'>
-        <div className='new-expense__control'>
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
           <label>Title</label>
-          <input type='text' name='title' onChange={userInputChangeHandler} value={userInput.title} />
+          <input
+            type="text"
+            name="title"
+            onChange={userInputChangeHandler}
+            value={userInput.title}
+          />
         </div>
-        <div className='new-expense__control'>
+        <div className="new-expense__control">
           <label>Amount</label>
-          <input type='number' name='amount' min='0.01' step='0.01' onChange={userInputChangeHandler} value={userInput.amount} />
+          <input
+            type="number"
+            name="amount"
+            min="0.01"
+            step="0.01"
+            onChange={userInputChangeHandler}
+            value={userInput.amount}
+          />
         </div>
-        <div className='new-expense__control'>
+        <div className="new-expense__control">
           <label>Date</label>
-          <input type='date' name='date' min='2019-01-01' max='2022-12-31' onChange={userInputChangeHandler} value={userInput.date} />
+          <input
+            type="date"
+            name="date"
+            min="2019-01-01"
+            max="2022-12-31"
+            onChange={userInputChangeHandler}
+            value={userInput.date}
+          />
         </div>
       </div>
-      <div className='new-expense__actions'>
-        <button type='button' onClick={cancelHandler}>Cancel</button>
-        <button type='submit' >Add Expense</button>
+      <div className="new-expense__actions">
+        <button type="button" onClick={cancelHandler}>
+          Cancel
+        </button>
+        <button type="submit">Add Expense</button>
       </div>
     </form>
   );
